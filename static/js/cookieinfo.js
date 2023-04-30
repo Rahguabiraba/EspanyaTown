6c6a2cb5633013fa63685467aebbbe937330e51c
Ramon Guabiraba <104869271+Rahguabiraba@users.noreply.github.com>
2023-04-30 11:13:19 +0200

    Actualización de estilos, cookies en paginas

---
+++ static/js/cookieinfo.js
@@ -0,0 +1,8 @@
+let cookieuser = document.getElementById("cookieuser");
+let cookiemail = document.getElementById("cookieemail");
+
+  //Declaración de cookies
+  const parrafo1 = document.createTextNode(user);
+  const parrafo2 = document.createTextNode(mail);
+  cookieuser.appendChild(parrafo1);
+  cookiemail.appendChild(parrafo2);
--- static/js/cookies.js
+++ static/js/cookies.js
@@ -1,29 +1,29 @@
-let usuario = document.getElementById("user")
-let correo = document.getElementById("email")
-let imagelogin = document.getElementById("logon")
-
-if (usuario.value != "" || correo.value != "") {
-  document.cookie = "username=" + usuario.value;
-  document.cookie = "correo=" + correo.value;
-} 
+//Declaración de variables
+let usuario = document.getElementById("user");
+let correo = document.getElementById("email");
+let imagelogin = document.getElementById("logon");
+const user = getCookie("username");
+const mail = getCookie("correo");
 
 
 //Verificar si existe la cookie
-let user = getCookie("username");
-let mail = getCookie("correo");
+if (usuario.value != "" || correo.value != "") {
+  document.cookie = "username=" + usuario.value;
+  document.cookie = "correo=" + correo.value;
+}
 
 //Si la cookie existe, entonces mostramos el icono del login
 if (user != "" || mail != "") {
-  imagelogin.style.display = "block"
-} 
+  imagelogin.style.display = "block";
+}
 
-
+//Función para coger la cookie como un string
 function getCookie(cname) {
   let name = cname + "=";
-  let ca = document.cookie.split(';');
-  for(let i = 0; i < ca.length; i++) {
+  let ca = document.cookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
     let c = ca[i];
-    while (c.charAt(0) == ' ') {
+    while (c.charAt(0) == " ") {
       c = c.substring(1);
     }
     if (c.indexOf(name) == 0) {
