26cfee905b3ba942e1aeeceac0e154a6e172387b
Ramon Guabiraba <104869271+Rahguabiraba@users.noreply.github.com>
2023-05-01 14:40:56 +0200

    Actualización de API

--- static/js/api.js
+++ static/js/api.js
@@ -1,12 +1,21 @@
 //Declaración de variables para uso de la consulta a la API
 const API_KEY = "fb58f15b056f0be58b18d8bc693ce131";
 const BASE_URL = "https://api.themoviedb.org/3/genre/";
+const IMG_URL = "https://image.tmdb.org/t/p/w500";
+
+
+//Generos que cogemos desde el formulario
 let genero = document.getElementById("genero").value;
+let contenedor = document.getElementById("contenedor");
+
+//Variables para la funcion de 3 numeros aleatorios
 var n = 0;
 var numero = 0;
 var uno = 0;
 var dos = 0;
 var tres = 0;
+
+//Array de clave/valor con el id de cada uno de los generos de peliculas
 const generos = {
   accion: 28,
   aventura: 12,
@@ -23,6 +32,7 @@ const generos = {
   romance: 10749,
 };
 
+//Creamos la URL de la api con base en los parametros
 const API_URL =
   BASE_URL +
   generos[genero] +
@@ -32,17 +42,83 @@ const API_URL =
 
 //Coger video youtube por pelicula pasando el ID
 // "https://api.themoviedb.org/3/movie/"+28+"/videos?api_key=fb58f15b056f0be58b18d8bc693ce131&language=en-US&include_adult=false&sort_by=created_at.asc"
+//Realizamos la consulta con la API
 fetch(API_URL)
   .then((res) => res.json())
   .then((data) => {
+    //Pasamos la informacion a una variable
     let peliculas = data.results;
+    //Utilizamos la funcion con numeros aleatorios
     let ids = numerosAleatorios();
+
     let listadoPeliculas = [];
     for (let contador = 0; contador < 3; contador++) {
+        //La lista de la API nos devuelve siempre 20 peliculas. Cogeremos 3 con la función
       listadoPeliculas.push(peliculas[ids[contador]]);
     }
-    console.log(listadoPeliculas);
+    //Para cada peliculas, iremos coger las informaciones
+    listadoPeliculas.forEach(movie => {
+        const {id,title,poster_path,vote_average,overview} = movie;
+        const VIDEO_URL = "https://api.themoviedb.org/3/movie/" + id + "/videos?api_key="+API_KEY+"&language=en-US&include_adult=false&sort_by=created_at.asc";
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('card');
+        movieEl.innerHTML = `
+        <div class="card-movies">
+          <div class="card__banner">
+            <div class="background">
+              <div class="c-background">
+                <div class="card__banner__content">
+                  <div class="__content">
+                    <p class="${getColor(vote_average)}">
+                      ${vote_average}
+                    </p>
+                    <button class="__content--btn" id="myBtn" onclick="${getTrailer(VIDEO_URL)}">
+                      Trailer
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <img
+              src="${IMG_URL + poster_path}"
+              alt="Banner"
+              class="banner"
+            />
+            <div class="drop">
+              <i class="fas fa-chevron-circle-down boton-drop">
+              </i>
+            </div>
+            <div class="card__descripcion" id="drop-descripcion">
+            <div class="c-card__descripcion">
+                <div class="card__descripcion__texto">
+                    <h1>${title}</h1>
+                    <p>${overview}</p>
+                </div>
+            </div>
+        </div>
+          </div>
+        </div>
+        `
+        //Añadimos la pelicula al contenedor
+        contenedor.appendChild(movieEl)
+    })
   });
+
+  function getTrailer(url) {
+    console.log(url)
+    // fetch(url)
+    // .then((res) => console.log(res.json()))
+  }
+
+  function getColor(vote) {
+    if (vote >= 8) {
+        return "green"
+    } else if(vote >= 5) {
+        return "orange"
+    } else {
+        return "red"
+    }
+  }
 
 //Funcion para coger 3 numeros aleatorios
 function numerosAleatorios() {
--- static/js/list.js
+++ static/js/list.js
@@ -1,16 +1,29 @@
-function buttonDrop() {
-  $(".boton-drop").toggleClass("drop-rotate");
-  $(".drop-descripcion").toggleClass("drop-active");
+// function buttonDrop() {
+//   $(".boton-drop").toggleClass("drop-rotate");
+//   $(".drop-descripcion").toggleClass("drop-active");
+// }
+
+// function activarEfecto(et) {
+
+// 	//necesito localizar la etiqueta <tr> de la fila que contiene la celda pulsada
+// 	let buttondrop = et.closest('.boton-drop')
+//   let buttondescripcion = et.closest('.drop-descripcion')
+
+//   buttondrop.classList.toggle("drop-rotate");
+//   buttondescripcion.classList.toggle("drop-active");
+// }
+
+let buttonsdrop = document.getElementsByClassName("boton-drop");
+
+for (let i=0;i<buttonsdrop.length;i++) {
+  buttonsdrop[i].addEventListener("click",desplegarDescripcion)
 }
 
-function buttonDrop2() {
-  $("#boton2-drop").toggleClass("drop-rotate");
-  $("#drop2-descripcion").toggleClass("drop-active");
-}
-
-function buttonDrop3() {
-  $("#boton3-drop").toggleClass("drop-rotate");
-  $("#drop3-descripcion").toggleClass("drop-active");
+function desplegarDescripcion(e) {
+  let button = e.target;
+  let descripcion = document.getElementById("drop-descripcion");
+  button.nodeChildren.classList.toggle("drop-rotate");
+  descripcion.classList.toggle("drop-active");
 }
 
 //Cogemos el dato de la variable y imprimimos en el alert
