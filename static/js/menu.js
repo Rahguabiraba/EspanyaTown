7674f8c234638da552631a010367698d2654f2b5
Ramon Guabiraba <104869271+Rahguabiraba@users.noreply.github.com>
2023-05-01 16:54:07 +0200

    Responsive Index

--- static/js/menu.js
+++ static/js/menu.js
@@ -18,3 +18,24 @@ function list() {
 function about() {
   location.replace("/acerca_de");
 }
+
+function abrirMenu() {
+  // Utilizar variables
+  icono = document.getElementById("menu");
+  menu = document.getElementById("myNav");
+  let links = document.getElementById("menu-outside");
+  linea1 = document.getElementById("bar1");
+  linea2 = document.getElementById("bar2");
+  linea3 = document.getElementById("bar3");
+
+  //Cambiamos el icono
+  icono.classList.toggle("change");
+
+  if (menu.style.width == "100%") {
+    menu.style.width = "0%";
+    links.style.display = "none";
+  } else {
+    menu.style.width = "100%";
+    links.style.display = "block";
+  }
+}
